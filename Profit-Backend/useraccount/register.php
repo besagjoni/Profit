2e4e390e506ee73fdd29f814353c7b8f164840a8
albinapopoci <64018471+albinapopoci@users.noreply.github.com>
2020-05-10 22:49:08 +0200

    Update register.php

--- Profit-Backend/useraccount/register.php
+++ Profit-Backend/useraccount/register.php
@@ -4,7 +4,7 @@
    <div class="container">
        <div class="row">
            <div class="col-lg-6 m-auto">
-               <div class="card bg-light mt-5 py-2">
+               <div class="card bg-light mt-5 py-2 photo">
                     <div class="card-title">
                         <h2 class="text-center mt-2"> Register</h2>
 						<hr>
