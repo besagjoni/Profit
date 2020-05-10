e47b73a5008e66001ac6e43920f51f78ef394916
albinapopoci <64018471+albinapopoci@users.noreply.github.com>
2020-05-10 22:48:41 +0200

    Update login.php

--- Profit-Backend/useraccount/login.php
+++ Profit-Backend/useraccount/login.php
@@ -4,7 +4,7 @@
    <div class="container">
        <div class="row">
            <div class="col-lg-6 m-auto">
-               <div class="card bg-light mt-5 py-2">
+               <div class="card bg-light mt-5 py-2 photo">
                     <div class="card-title">
                     <?php 
                         display_message(); 
