d0f76e0a2b98c242d7300f99b1f64a602e0e6b31
besagjoni <64018097+besagjoni@users.noreply.github.com>
2020-05-24 21:10:14 +0200

    Update nutrition.php

--- Profit-Backend/nutrition.php
+++ Profit-Backend/nutrition.php
@@ -16,31 +16,30 @@
 </head>
 
 <body data-spy="scroll" data-target="#navbarResponsive">
-<?php
+    <?php
 
-//database connection
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db = 'profitphpmyadmin';
+    //database connection
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $db = 'profitphpmyadmin';
 
-$mysqli = new mysqli($host, $user, $pass, $db);
+    $mysqli = new mysqli($host, $user, $pass, $db);
 
-$result = $mysqli->query ("SELECT * FROM nutrition ORDER BY Title DESC LIMIT 5") or die($mysqli->error);
-$result2 = $mysqli->query ("SELECT * FROM nutritioncategory ORDER BY Name ASC") or die($mysqli->error);
-
-$ID= $_GET['ID'];
-$Title='';
-$Description='';
-$NutritionCategory='';
-
-?>
-
+    $result = $mysqli->query ("SELECT * FROM program ORDER BY Title DESC LIMIT 5") or die($mysqli->error);
+    $result2 = $mysqli->query ("SELECT * FROM programcategory ORDER BY Name ASC") or die($mysqli->error);
+    
+    $ID= $_GET['ID'];
+    $Title='';
+    $Description='';
+    $ProgramCategory='';
+    
+    ?>
     <!--Start Home Section-->
     <div id="home">
 
-       <!--Navigation-->
-       <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <!--Navigation-->
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
             <a class="navbar-brand" href="index.php"><img src="img/logo.PNG"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
                 <span class="navbar-toggler-icon"> </span>
@@ -51,10 +50,10 @@ $NutritionCategory='';
                         <a class="nav-link" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="programs.php">Programs</a>
+                        <a class="nav-link active" href="programs.php">Programs</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="nutrition.php">Nutrition</a>
+                        <a class="nav-link" href="nutrition.php">Nutrition</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="index.php#contact-area">Contact</a>
@@ -67,9 +66,9 @@ $NutritionCategory='';
                     </li>
                 </ul>
                 <div class="search">
-                    <form action="search.php" method="POST">
+                    <form action="#">
                         <input type="text" placeholder=" Search... " name="search">
-                        <button name="submit-search">
+                        <button>
                             <i class="fa fa-search" style="font-size: 18px;">
                             </i>
                         </button>
@@ -84,46 +83,42 @@ $NutritionCategory='';
         <div class="header-img">
             <img src="img/hdr.jpg" class="responsive">
         </div>
-
-
-        <!--HEADER IMAGE-->
-
     </div>
     <!--End Home Section-->
 
 
-    <div class="row" id="all-nutrition">
+    <div class="row" id="all-programs">
         <div class="column left">
             <div class="row programs-title">
-                <h3>Nutrition</h3>
+                <h3>Programs</h3>
             </div>
             <div class="row programs-container">
-                <?php while ($nutrition = $result->fetch_assoc()): ?> 
+                <?php while ($program = $result->fetch_assoc()): ?> 
                 <div class="row programs-content">
                     <div class="card mb-3 program-card" style="max-width: 540px;">
                         <div class="row no-gutters">
                             <div class="col-md-4">
-                            <?php echo '<img src="data:img/jpg;base64,'.base64_encode($nutrition['img']).'" height="200" width="200"  class="card-img" alt="..." >' ?>
+                            <?php echo '<img src="data:img/jpg;base64,'.base64_encode($program['img']).'" height="200" width="200"  class="card-img" alt="..." >' ?>
                             </div>
                             <div class="col-md-8">
                                 <div class="card-body">
-                                    <h5 class="card-title"><?= $nutrition['Title'] ?></h5>
+                                    <h5 class="card-title"><?= $program['Title'] ?></h5>
                                 </div>
                                 <div class="card-body">
                                     <a class="card-text font-italic">Category:</a>
-                                    <?= $nutrition['NutritionCategory'] ?>
+                                    <?= $program['ProgramCategory'] ?>
                                 </div>
                                 <div class="card-body program-action">
-                                    <a href="useraccount/redirect.php?ID=<?php echo $nutrition['ID']; ?>" class="card-link full-item" >See full article</a>
-                                    <button type="button" class="btn btn-outline-warning save-btn"name="save"><a href="saved-items.php" >Save</a></button>
+                                    <a href="useraccount/redirect.php?ID=<?php echo $program['ID']; ?>" class="card-link full-item" >See full article</a>
+                                    <button type="button" class="btn btn-outline-warning save-btn savePost" value="<?php echo $program['ID']; ?>"><a href= "useraccount/redirectsaved.php?ID=<?php echo $program['ID']; ?>">Save</a></button>
                                 </div>
                             </div>
-
                         </div>
                     </div>
                 </div>
                 <?php endwhile; ?>
             </div>
+    
             <nav aria-label="Page navigation example">
                 <ul class="pagination justify-content-center">
                     <li class="page-item disabled">
@@ -142,43 +137,45 @@ $NutritionCategory='';
             <div class="row categories-title">
                 <h4>Categories</h4>
             </div>
-            <div class="border-bottom"><?php while ($category = $result2->fetch_assoc()): ?></div>
-            <div class="row categories-container">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item"><a href=""><?= $category['Name'] ?></a></li>
-                </ul>
-            <?php endwhile; ?>
-            </div>
+            <div class="border-bottom"><?php while ($category = $result2->fetch_assoc()): ?> </div>
+                <div class="row categories-container">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item" name="kategori"><a href=""><?= $category['Name'] ?></a></li>
+                    </ul> 
+                    <?php endwhile; ?>       
+                </div>    
+            </div>                 
         </div>
     </div>
 
 
-    <!--Start Footer Section-->
-    <div id="footer" class="offset">
 
-        <footer>
-            <div class="row justify-content-center">
+        <!--Start Footer Section-->
+        <div id="footer" class="offset">
 
-                <div class="col-md-5 text-center">
-                    <img src="img/logo.png">
-                    <p>Gjithmone prane jush per t'ju sjelle programet e fitnesit dhe dietat me te mira qe jane cerifikuar deri me tani.</p>
+            <footer>
+                <div class="row justify-content-center">
 
-                    <a href="https://facebook.com" target="_blank"><i class="fab fa-facebook-square"></i></a>
-                    <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter-square"></i></a>
-                    <a href="https://instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+                    <div class="col-md-5 text-center">
+                        <img src="img/logo.png">
+                        <p>Gjithmone prane jush per t'ju sjelle programet e fitnesit dhe dietat me te mira qe jane cerifikuar deri me tani.</p>
+
+                        <a href="https://facebook.com" target="_blank"><i class="fab fa-facebook-square"></i></a>
+                        <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter-square"></i></a>
+                        <a href="https://instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+
+                    </div>
+
+                    <hr class="socket">
+                    &copy; ProFIT.
+
 
                 </div>
+                <!--End of Row-->
+            </footer>
 
-                <hr class="socket">
-                &copy; ProFIT.
-
-
-            </div>
-            <!--End of Row-->
-        </footer>
-
-    </div>
-    <!--End footer Section-->
+        </div>
+        <!--End footer Section-->
 
 
 
@@ -188,7 +185,27 @@ $NutritionCategory='';
     <script src="https://use.fontawesome.com/releases/v5.5.0/js/all.js"></script>
     <!--- End of Script Source Files -->
 
+    <script>
+    $(document).ready(function(){
+        $('.savePost').on('click', function(){
+        var statusVal = $(this).val();
+        console.log(statusVal);
+            $.ajax({
+                type: "POST",
+                url: "saveStatus.php",
+                data: {statusType: statusVal},
+                success: function(msg){
+                    console.log(msg);
+                }
+            })
+        });
+    });
+
+    </script>
+    <!-- fundi i save -->
+
 
 </body>
 
 </html>
+
